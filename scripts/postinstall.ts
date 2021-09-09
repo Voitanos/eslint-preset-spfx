ce46c90d44b6a5387f688c8eab149f08929292cd
jonah <falsefriend13@gmail.com>
2021-09-09 15:51:46 -0400

    added an if statement to check for the tslint.json file before trying to delete. If no file, skip.

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -130,5 +130,5 @@ console.log(`INFO: Deleting tslint.info unnecessary file`);
 
 const TSLINT_FILEPATH = path.resolve(path.join(projectPath, 'tslint.json'));
 
-// delete file
-fs.unlinkSync(TSLINT_FILEPATH);
+// delete file (if it exists)
+if (fs.existsSync(TSLINT_FILEPATH)) { fs.unlinkSync(TSLINT_FILEPATH); }
